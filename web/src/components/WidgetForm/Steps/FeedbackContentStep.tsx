b659744df153f2d779ef90760f4ddadd35f71f94
AndersonPGS <andersonpgs2001@hotmail.com>
2022-05-16 17:32:12 -0300

    ⏹️ Aula  Finished - Success Page created

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,11 +7,13 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
+  onFeedbackSent,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
 
@@ -25,6 +27,8 @@ export function FeedbackContentStep({
       screenshot,
       comment,
     });
+
+    onFeedbackSent();
   }
 
   return (
@@ -64,7 +68,7 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0 ? false : true}
+            disabled={comment.length === 0 ? true : false}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
             Enviar feedback
