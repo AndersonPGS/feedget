8f0c6c17e52bd9ca63c1bed84820afd7bf8b6d82
AndersonPGS <andersonpgs2001@hotmail.com>
2022-05-23 22:25:03 -0300

    Aula 3 Finished - Testes unitários

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "15f9bac9a689a3",
+    pass: "7a4bc142de8812",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Anderson Paiva <andersonpgs2001@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
